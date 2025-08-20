b29454d8d1a0b3c1feee593f3ee98ecc7263fcfe
LewieJ08 <lewiej08@gmail.com>
2025-08-20 21:18:25 +0100

    feat: import userRoutes to entry point

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const loggerMiddleware = require("./middleware/logger");
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(loggerMiddleware);
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
