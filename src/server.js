db05d3782ca5c5288fc5825ca6da84775d08eb3f
LewieJ08 <lewiej08@gmail.com>
2025-08-21 20:12:43 +0100

    fix: update middleware use order in server.js

--- src/server.js
+++ src/server.js
@@ -9,9 +9,11 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(mwLogger);
-app.use(mwErrorHandler);
 app.use("/users", userRoutes);
+
+app.use(mwErrorHandler);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`);
