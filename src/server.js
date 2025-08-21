44d9a8e9b1c8289df7dfaab935cf78f8654883b8
LewieJ08 <lewiej08@gmail.com>
2025-08-21 01:53:26 +0100

    feat: import db connection func

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const loggerMiddleware = require("./middleware/logger");
+const connectDB = require("./config/db")
+const mwLogger = require("./middleware/logger");
+const mwErrorHandler = require("./middleware/errorHandler");
 const userRoutes = require("./routes/userRoutes");
 
 require("dotenv").config();
 const app = express();
+connectDB();
 
 app.use(express.json());
-app.use(loggerMiddleware);
+app.use(mwLogger);
+app.use(mwErrorHandler);
 app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port: ${process.env.PORT}`);
 });
 
