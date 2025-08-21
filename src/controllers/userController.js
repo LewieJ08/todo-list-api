5a14b245a40c12e76b87426ff6556164fd860b46
LewieJ08 <lewiej08@gmail.com>
2025-08-21 01:54:38 +0100

    feat: add createUser func in controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,22 @@
-const getUsers = async (req, res, next) => {
-    res.send("GET all users")
-    console.log("Get all users");
+const User = require("../models/user")
+
+const getUsers = async (req, res, next) => {}
+
+const createUser = async (req, res, next) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully.",
+            data: user
+        });
+    } catch(error) {
+        next(error);
+    }
 }
 
-module.exports = {getUsers};
+
+const loginUser = async () => {}
+
+module.exports = {getUsers, createUser, loginUser};
