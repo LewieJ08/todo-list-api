ba44bcf7891bec4957c9479a6953e846daa1ab15
LewieJ08 <lewiej08@gmail.com>
2025-08-26 16:18:36 +0100

    feat: define /users/logout route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get("/", userController.getUsers);
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
+router.post("/logout", userController.logoutUser)
 
 module.exports = router;
