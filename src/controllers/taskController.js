b2f33a0b4eceade158a23540e8fa8ca37cf249ea
LewieJ08 <lewiej08@gmail.com>
2025-08-27 20:41:02 +0100

    feat: initial controller setup

---
+++ src/controllers/taskController.js
@@ -0,0 +1,24 @@
+const Task = require("../models/task");
+
+const getAllTasks = (req, res, next) => {
+    // pass
+}
+
+const getTask = (req, res, next) => {
+    // pass
+}
+
+const createTask = (req, res, next) => {
+    //pass
+}
+
+const updateTask = (req, res, next) => {
+    //pass
+}
+
+const deleteTask = (req, res, next) => {
+    //pass
+}
+
+
+module.exports = {getAllTasks, getTask, createTask, updateTask, deleteTask}
