8fb4cd3f8f08d0096d840dca725caafaa6e60fbc
LewieJ08 <lewiej08@gmail.com>
2025-08-27 17:21:29 +0100

    feat: import auth middleware for route use

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const mwAuth = require("../middleware/auth")
 
 const router = express.Router();
 
 router.get("/", userController.getUsers);
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
-router.post("/logout", userController.logoutUser)
+router.post("/logout", mwAuth, userController.logoutUser)
 
 module.exports = router;
