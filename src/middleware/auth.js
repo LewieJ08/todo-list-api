467486688ab9aaa4d635e1c43117fd3144a3c7d9
LewieJ08 <lewiej08@gmail.com>
2025-08-27 17:20:56 +0100

    feat: add auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const User = require("../models/user")
+
+module.exports = async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).json({
+                success: false,
+                error: "Missing authorization token"
+            });
+        }
+
+        const token = (req.headers.authorization).split(" ")[1];
+        const user = await User.findOne({token: token});
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: `Invalid authorization token`
+            });
+        }
+    
+        next();
+    } catch(error) {
+        next(error);
+    }
+}
