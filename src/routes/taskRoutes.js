ae829325f0017eaa280f97e67eb521a338ecebf5
LewieJ08 <lewiej08@gmail.com>
2025-08-27 20:30:06 +0100

    feat: add controller functions and auth middleware

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const taskController = require("../controllers/taskController");
+const mwAuth = require("../middleware/auth");
+
+const router = express.Router;
+router.use(mwAuth);
+
+router.route("/")
+    .get(taskController.getAllTasks)
+    .post(taskController.createTask);
+
+router.route("/:id")
+    .get(taskController.getTask)
+    .put(taskController.updateTask)
+    .delete(taskController.deleteTask);
