f9b738c09f0d0856b56d7db8c3d5ce803a3c741e
LewieJ08 <lewiej08@gmail.com>
2025-08-28 16:31:55 +0100

    fix: update router method

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const taskController = require("../controllers/taskController");
 const mwAuth = require("../middleware/auth");
 
-const router = express.Router;
+const router = express.Router();
 router.use(mwAuth);
 
 router.route("/")
