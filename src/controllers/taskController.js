d7be6f7d982a2afa4d41869886d4bce48525f20d
LewieJ08 <lewiej08@gmail.com>
2025-08-28 16:33:15 +0100

    feat: add createTask function

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -8,8 +8,18 @@ const getTask = (req, res, next) => {
     // pass
 }
 
-const createTask = (req, res, next) => {
-    //pass
+const createTask = async (req, res, next) => {
+    try {
+        const task = new Task({_userId: req.user._id, ...req.body});
+        await task.save()
+        res.status(201).json({
+            success: true,
+            message: `Task created successfully by ${req.user.username}`,
+            data: task
+        })
+    } catch(error) {
+        next(error);
+    }
 }
 
 const updateTask = (req, res, next) => {
