e2adbef3715621f656e4c91cfc17a7d2af3d63a4
LewieJ08 <lewiej08@gmail.com>
2025-08-28 17:12:40 +0100

    feat: add getAllTasks func in task controller

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,33 +1,54 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res, next) => {
-    // pass
+const getAllTasks = async (req, res, next) => {
+    try {
+        const tasks = await Task.find({_userId: req.user._id});
+        res.status(200).json({
+            success: true,
+            message: `Tasks for ${req.user.username} fetched successfully`,
+            data: tasks
+        });
+    } catch(error) {
+        next(error);
+    }
 }
 
 const getTask = (req, res, next) => {
-    // pass
+    try {
+
+    } catch(error) {
+        next(error);
+    }
 }
 
 const createTask = async (req, res, next) => {
     try {
         const task = new Task({_userId: req.user._id, ...req.body});
-        await task.save()
+        await task.save();
         res.status(201).json({
             success: true,
             message: `Task created successfully by ${req.user.username}`,
             data: task
-        })
+        });
     } catch(error) {
         next(error);
     }
 }
 
 const updateTask = (req, res, next) => {
-    //pass
+    try {
+
+    } catch(error) {
+        next(error);
+    }
 }
 
 const deleteTask = (req, res, next) => {
-    //pass
+    try {
+
+    } catch(error) {
+        next(error);
+    }
 }
 
 
