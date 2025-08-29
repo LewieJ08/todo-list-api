8ec84a28c9a9d63f9b4192a7a3d8a4a73b40ae9b
LewieJ08 <lewiej08@gmail.com>
2025-08-29 21:33:13 +0100

    feat: import idChecker middleware

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const taskController = require("../controllers/taskController");
 const mwAuth = require("../middleware/auth");
+const mwIdChecker = require("../middleware/idChecker");
 
 const router = express.Router();
 router.use(mwAuth);
@@ -10,8 +11,8 @@ router.route("/")
     .post(taskController.createTask);
 
 router.route("/:id")
-    .get(taskController.getTask)
-    .put(taskController.updateTask)
-    .delete(taskController.deleteTask);
+    .get(mwIdChecker, taskController.getTask)
+    .put(mwIdChecker, taskController.updateTask)
+    .delete(mwIdChecker, taskController.deleteTask);
 
-module.exports = router
+module.exports = router;
